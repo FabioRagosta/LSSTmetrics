b2d7dc36cdcff2cdfa5975279eae5876ed50c2fd
FabioRagosta <53743894+FabioRagosta@users.noreply.github.com>
2021-12-10 12:20:28 +0100

    Update astrometry_metrics.py

--- metrics and documentation/astrometry_metrics.py
+++ metrics and documentation/astrometry_metrics.py
@@ -174,16 +174,6 @@ class LSPMmetric(BaseMetric):
                     U=np.arange(-100,100,25),V=np.arange(-100,100,25),W=np.arange(-100,100,25),unusual='uniform',treshold='1d',m5Col='fiveSigmaDepth',  
                     mjdCol='observationStartMJD',filterCol='filter', seeingCol='seeingFwhmGeom', nexp= 1,dataout=False, 
                     **kwargs):
-            '''
-        mjdCol= MJD observations column name from Opsim database      (DEFAULT = observationStartMJD) 
-        m5Col= Magnitude limit column name from Opsim database      (DEFAULT = fiveSigmaDepth)
-        filterCol= Filters column name from Opsim database      (DEFAULT = filter)
-        seeingCol = "Geometrical" full-width at half maximum.  (DEFAULT = seeingFwhmGeom)
-        f = filter for the observation
-        snr_lim = threshold for the signal to noise ratio   
-        percentiles = percentile limits to separate known motion from the anomalous one. 
-        
-            '''
             self.mjdCol = mjdCol 
             self.m5Col = m5Col 
             self.seeingCol = seeingCol 
@@ -197,6 +187,7 @@ class LSPMmetric(BaseMetric):
             self.U=U 
             self.V=V 
             self.W=W 
+            self.unusual=unusual
             self.treshold=treshold
             self.dataout = dataout 
              # to have as output all the simulated observed data set dataout=True, otherwise the relative error for  
@@ -214,7 +205,67 @@ class LSPMmetric(BaseMetric):
             np.seterr(over='ignore',invalid='ignore')
          # typical velocity distribution from litterature (Binney et Tremain- Galactic Dynamics)
             
-                         
+        def position_selection(self,R,z):
+            # costants
+            ab = 1 #kpc
+            ah = 1.9 #kpc
+            alphab = 1.8
+            alphah = 1.63
+            betah =2.17
+            rb = 1.9 #Kpc
+            qb = 0.6
+            qh = 0.8
+            z0 = 0.3 #kpc
+            z1 = 1 #kpc
+            Rd = 3.2 #kpc
+            rho0b = 0.3*10**(-9) #Mskpc^-3
+            rho0h = 0.27*10**(-9) #Mskpc^-3
+            sigma = 300*10**(-6) #Mskpc^-2
+            alpha1 = 0.5
+            # parametes
+            mb = np.sqrt(R**2+z**2/qb**2)
+            mh = np.sqrt(R**2+z**2/qh**2)
+            alpha0 =1-alpha1
+            
+            rhoh = rho0h*(mh/ah)**alphah*(1+mh/ah)**(alphah-betah)
+            rhob = rho0b*(mb/ab)**(-alphab)*np.exp(-mb/rb)
+            rhod = sigma*np.exp(R/Rd)*(alpha0/(2*z0)*np.exp(-np.absolute(z)/z0)+alpha1/(2*z1)*np.exp(-np.absolute(z)/z1))
+            densities = np.array([rhoh,rhob,rhod])/np.sum(np.array([rhoh,rhob,rhod]))
+            mode = np.array(['H','B','D'])
+            idx = np.where(densities == np.amax(densities))
+            return mode[idx]
+        
+        def DF(self,V_matrix,mode,R,z):                
+            if mode == 'H':
+                v = np.sqrt(V_matrix[0,:]**2+V_matrix[1,:]**2+V_matrix[2,:]**2)
+                vesc = 575 #km/s
+                vsun =187.5 #km/s
+                N=1.003
+                P=4*N/vsun/np.sqrt(np.pi)*(v/vesc)**2*np.exp(-v**2/vsun)
+            if mode == 'B': 
+                v = np.sqrt(V_matrix[0,:]**2+V_matrix[1,:]**2+V_matrix[2,:]**2)
+                disp = 140 #km/s
+                P = np.exp(-v**2/2/disp**2)/np.sqrt(np.pi)/disp
+            if mode == 'D':
+                k= 0.25
+                q =0.45
+                Rd = 3.2 #kpc
+                sigmaz0= 19 #km/s
+                sigmar0= 33.5 #km/s
+                beta = 0.33
+                L0 = 10 #km/s
+                sigma = 300*10**(-6) #Ms/kpc**2
+            
+                # parameters
+                v = V_matrix[2,:]
+                Jz = v**2/2 + v**2/2*np.log(R**2+z**2/0.8**2) 
+                Ar = k * Jz /np.exp(2*q*(120 - R)/Rd)/sigmar0
+                Az =  k * Jz /np.exp(2*q*(120 - R)/Rd)/sigmaz0                
+                P = v**2/Jz*sigma*(1+np.tanh(R*v/L0))*np.exp(-k*Jz/(sigmar0*np.exp(2*q*(120 - R)/Rd))**2)/(np.pi*k*sigmar0*np.exp(2*q*(120 - R)/Rd))
+                
+            return P
+          
+                           
         def run(self, dataSlice, slicePoint=None): 
             np.random.seed(2500) 
             obs = np.where((dataSlice['filter']==self.f) & (dataSlice[self.mjdCol]<min(dataSlice[self.mjdCol])+365*self.surveyduration)) 
@@ -223,6 +274,7 @@ class LSPMmetric(BaseMetric):
             mu = np.array(self.simobj['PM'])
             muf = np.array(self.simobj['PM_out'])
             mjd = dataSlice[self.mjdCol][obs]
+            fwhm = dataSlice[self.seeingCol][obs]
             if len(dataSlice[self.m5Col][obs])>2: 
                 
                 # select objects above the limit magnitude threshold 
@@ -241,7 +293,7 @@ class LSPMmetric(BaseMetric):
                 DeltaTs.sort()
                 DeltaTs = np.array(DeltaTs)
                 if np.size(DeltaTs)>0:
-                    dt_pm = 0.05*np.median(dataSlice[self.seeingCol])/muf[np.unique(row)]
+                    dt_pm = 0.05*np.median(fwhm)/muf[np.unique(row)]
                     selection = np.where((dt_pm>DeltaTs[0]) & (dt_pm<DeltaTs[-1]))
 
                     if np.size(selection)>0:
@@ -249,7 +301,16 @@ class LSPMmetric(BaseMetric):
                         pm_alpha, pm_delta = mu[selection]*np.sin(pa), mu[selection]*np.cos(pa)
                         pm_un_alpha, pm_un_delta = muf[selection]*np.sin(pa), muf[selection]*np.cos(pa)                    
                         p_min,p_max,p_mean = self.percentiles[0],self.percentiles[1],self.percentiles[2]
-                        if self.treshold =='1d':
+                        if self.treshold=='conv':
+                            Pm = np.exp(-((0.05*np.nanmedian(fwhm)-np.array(mu)[selection]*np.array(dt_pm)[selection,np.newaxis])/(0.67*np.nanmedian(fwhm))*np.nanmedian(snr[selection], axis=1))**2)
+                            L = norm.pdf(mu[selection])
+                            L_out = norm.pdf(muf[selection])
+                            Pm_out = np.exp(-((0.05*np.median(fwhm)-np.array(muf)[selection]*np.array(dt_pm)[selection,np.newaxis])/(0.67*np.nanmedian(fwhm))*np.nanmedian(snr[selection], axis=1))**2)
+                            l_frac = np.log(signal.fftconvolve(L_out,Pm_out.flatten()))-np.log(signal.fftconvolve(L,Pm.flatten()))                      
+                            #l_frac = np.log(np.abs(scipy.fft(L_out)*scipy.fft(Pm_out.flatten())))-np.log(np.abs(scipy.fft(L)*scipy.fft(Pm.flatten())))                                                  
+                            unusual= np.shape(np.where(l_frac[~np.isnan(l_frac)]>0))[1]
+                            res= np.size(unusual)/np.size(selection)
+                        elif self.treshold =='1d':
                             mu_min,mu_max, mu_mean = np.percentile(np.array(mu)[selection],[p_min,p_max,p_mean])
                             OK = np.isfinite(np.array(muf)[selection])
                             pm = muf[selection][OK]
@@ -289,7 +350,7 @@ class LSPMmetric(BaseMetric):
                         if self.dataout:
                             return dic 
                         else: 
-                            return res
+                            return res 
           
           
 
